c22bda9a5308db4ed67321c6d4fdb32ef1de9fb1
MatthewBurke <matthew.burke@oracle.com>
2018-03-07 19:07:21 -0800

    support for 1d arrays

--- psi.py
+++ psi.py
@@ -67,25 +67,29 @@ def calculate_psi(expected, actual, buckets=10, axis=0):
     
         def sub_psi(e_perc, a_perc):
             '''Calculate the actual PSI value from comparing the values.
-               Return zero if no values in actual
+               Update the actual value to a very small number if equal to zero
             '''
+            if a_perc == 0:
+                a_perc = 0.001
+            
             value = (e_perc - a_perc) * np.log(e_perc / a_perc)
-            
-            if np.isinf(value):
-                value = 0 
-            
             return(value)
         
         psi_value = np.sum(sub_psi(expected_percents[i], actual_percents[i]) for i in range(0, len(expected_percents)))
 
         return(psi_value)
     
-    psi_values = np.empty(expected.shape[axis])
+    if len(expected.shape) == 1:
+        psi_values = np.empty(len(expected.shape))
+    else:
+        psi_values = np.empty(expected.shape[axis])
     
     for i in range(0, len(psi_values)):
-        if axis == 0:
+        if len(psi_values) == 1:
+            psi_values = psi(expected, actual, buckets)
+        elif axis == 0:
             psi_values[i] = psi(expected[:,i], actual[:,i], buckets)
         elif axis == 1:
             psi_values[i] = psi(expected[i,:], actual[i,:], buckets)
     
-    return(psi_values / 100.)
+    return(psi_values)
